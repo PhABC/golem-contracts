c7a4dde2f2a525e43576c385c55e3b3d46e25fb1
PhABC <ph.castonguay@gmail.com>
2018-04-11 17:08:47 -0400

    Adding test to show deposit problem and truffle related scripts

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,9 @@
+// NOTE: Use this file to easily deploy the contracts you're writing.
+//   (but make sure to reset this file before committing
+//    with `git checkout HEAD -- migrations/2_deploy_contracts.js`)
+
+//var C = artifacts.require('C');
+
+module.exports = function (deployer, network) {
+//
+};
